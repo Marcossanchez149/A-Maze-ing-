741d1b425077d5d034933c0b6cd4b1c349dd1a3d
Kirill <kirilljiayou93@gmail.com>
2026-02-15 11:15:47 +0100

    Exporting constants

--- config/__init__.py
+++ config/__init__.py
@@ -8,5 +8,7 @@ __version__ = "1.0.0"
 from .parser import parse_config_file
 from .validator import validate_config
 from .types import ConfigDict, ConfigValue
+from .constants import REQUIRED_KEYS
 
-__all__ = ["parse_config_file", "validate_config", "ConfigDict", "ConfigValue"]
+__all__ = ["parse_config_file", "validate_config",
+           "ConfigDict", "ConfigValue", "REQUIRED_KEYS"]
