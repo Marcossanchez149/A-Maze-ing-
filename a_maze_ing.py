c87e152a7eabafe21981fb98d03e8f54283a43de
Kirill <kirilljiayou93@gmail.com>
2026-02-15 17:16:42 +0100

    Little try and catch for main testing

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -6,22 +6,25 @@ from core.maze import Maze
 
 def main():
     print("A-Maze-Ing\n")
-    config = parse_config_file("config.txt")
-    config = validate_config(config)
+    try:
+        config = parse_config_file("config.txt")
+        config = validate_config(config)
 
-    print("-------Config-------")
-    print(config)
-    print()
-    width = config.get("width", 10)
-    height = config.get("height", 10)
+        print("-------Config-------")
+        print(config)
+        print()
+        width = config.get("width", 10)
+        height = config.get("height", 10)
 
-    maze = Maze(
-        width=width,
-        height=height,
-    )
+        maze = Maze(
+            width=width,
+            height=height,
+        )
 
-    print("Maze in HEX:")
-    maze.print_hex()
+        print("Maze in HEX:")
+        maze.print_hex()
+    except ValueError as e:
+        print(e)
 
 if __name__ == "__main__":
     main()
