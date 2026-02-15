ba6aa2ac12d8df37726e9fd7b7db8cfe75d4d8d4
Kirill <kirilljiayou93@gmail.com>
2026-02-15 09:44:59 +0100

    Add abstract render class

---
+++ render/render.py
@@ -0,0 +1,40 @@
+"""
+render.py
+Defines contract for render
+"""
+
+
+from abc import ABC, abstractmethod
+
+
+class Render(ABC):
+    """
+    Render
+    Main Render class
+    """
+    @abstractmethod
+    def draw_maze(self, maze):
+        """
+        Draw a maze
+
+        :param self: own instance
+        :param maze: maze to draw
+        """
+        pass
+
+    @abstractmethod
+    def draw_cell(self, cell):
+        """
+        Draw a cell
+
+        :param self: own instance
+        :param cell: cell to draw
+        """
+
+    @abstractmethod
+    def clear(self):
+        """
+        Clear the screen
+
+        :param self: own instance
+        """
